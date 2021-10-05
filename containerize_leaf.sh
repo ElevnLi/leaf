7d70268f0f92aca1ca51959497c19e5f89572743
Elevn Li <zheng.li411@gmail.com>
2021-10-05 12:23:01 +1100

    fix docker container config of sqlcmd

--- containerize_leaf.sh
+++ containerize_leaf.sh
@@ -3,14 +3,15 @@
 SA_PASSWORD=Th3PA55--8zz       # DB password
 DB_SERVER=host.docker.internal # Use 'host.docker.internal' if mac, else '127.0.0.1' if linux 
                                # see: (https://stackoverflow.com/questions/24319662/from-inside-of-a-docker-container-how-do-i-connect-to-the-localhost-of-the-mach)
-LEAF_ROOT=$PWD                      
+LEAF_ROOT=$PWD
+LEAF_JWT_CERT="$PWD"/keys/cert.pem
+LEAF_JWT_KEY="$PWD"/keys/key.pem
+LEAF_JWT_KEY_PW=password
 
 # Check ENV variables
-if [ -z ${LEAFDB_SA_PW+x}    ]; then echo "LEAFDB_SA_PW is unset!"    exit; fi
+if [ -z ${SA_PASSWORD+x}    ]; then echo "LEAFDB_SA_PW is unset!"    exit; fi
 if [ -z ${LEAF_JWT_CERT+x}   ]; then echo "LEAF_JWT_CERT is unset!"   exit; fi
 if [ -z ${LEAF_JWT_KEY+x}    ]; then echo "LEAF_JWT_KEY is unset!"    exit; fi
-if [ -z ${LEAF_JWT_KEY+x}    ]; then echo "LEAF_JWT_KEY is unset!"    exit; fi
-if [ -z ${LEAF_JWT_KEY_PW+x} ]; then echo "LEAF_JWT_KEY_PW is unset!" exit; fi
 if [ -z ${LEAF_JWT_KEY_PW+x} ]; then echo "LEAF_JWT_KEY_PW is unset!" exit; fi
 
 # Extract cert/key path from ENVs
@@ -22,10 +23,20 @@ if [ -z ${KEYS_PATH+x} ]; then echo "Couldn't find cert+key path! Are you sure L
 #--------------
 docker run -d -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SA_PASSWORD" -p 1433:1433 --name leaf_db_demo mcr.microsoft.com/mssql/server:2017-latest 
 sleep 10s
-# sqlcmd oddly prefers '127.0.0.1' (not 'host.docker.internal'), so use that here
-sqlcmd -S '127.0.0.1' -U SA -P $SA_PASSWORD -i src/db/build/LeafDB.sql 
-sqlcmd -S '127.0.0.1' -U SA -P $SA_PASSWORD -d LeafDB -i src/db/build/LeafDB.Init.sql
-sqlcmd -S '127.0.0.1' -U SA -P $SA_PASSWORD -i src/db/build/TestDB.sql
+
+docker_sqlcmd() {
+    path="$1"
+    shift  # get the remaining arguments
+    docker run \
+      -v "$PWD"/src/db/build/:/sql \
+      mcr.microsoft.com/mssql-tools \
+      /opt/mssql-tools/bin/sqlcmd -S 'host.docker.internal' -U SA -P "$SA_PASSWORD" "$@" -i /sql/"$path"
+}
+
+
+docker_sqlcmd LeafDB.sql
+docker_sqlcmd LeafDB.Init.sql -d LeafDB
+docker_sqlcmd TestDB.sql
 
 #--------------
 # API
